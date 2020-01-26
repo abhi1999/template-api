07ea5ad4346a3be4f69da80c49f9eded237b507b
abhishek <abhi1999@gmail.com>
2020-01-25 23:24:17 -0500

    sample for auth0

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
 dotenv.config();
+
 const responseTimeLogger = require('./utils/responseTimeLogger');
 const routes = require('./routes');
 const {initDb} = require('./utils/dbUtils');
@@ -25,6 +26,9 @@ const {initDb} = require('./utils/dbUtils');
 const jwt = require('express-jwt');
 const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
+
+
+  
 
 var app = express();
 
@@ -40,6 +44,37 @@ app.use(bodyParser.urlencoded({limit:'50mb', extended:true}))
 
 app.use(bodyParser.json({limit:'50mb'}));
 
+app.use('/api', routes)
+
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+    throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
+  }
+  
+  const checkJwt = jwt({
+    // Dynamically provide a signing key based on the [Key ID](https://tools.ietf.org/html/rfc7515#section-4.1.4) header parameter ("kid") and the signing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+  
+    // Validate the audience and the issuer.
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+    algorithms: ['RS256']
+  });
+  
+  const checkScopes = jwtAuthz(['read:messages']);
+
+  app.get('/api/private', checkJwt, function(req, res) {
+    res.json({
+      message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+    });
+  });
+
+  
 initDb().then(()=>{
     // Initialize the app.
     var server = app.listen(process.env.PORT || 8080, function () {
@@ -50,6 +85,3 @@ initDb().then(()=>{
     appLogger.error(err);
     process.exit(1);
 })
-
-app.use('/api', routes)
- 
